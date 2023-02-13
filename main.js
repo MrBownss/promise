d989b284065f128936cae39eb97c5acaf994af6e
MrBownss <ahmadtaufik4599@gmail.com>
2023-02-14 03:02:39 +0700

    add promise and card

--- main.js
+++ main.js
@@ -1,23 +1,29 @@
+let card = document.getElementById('card');
+let bar = document.getElementById('searchBar');
+const apis = 'https://newsapi.org/v2/top-headlines?country=id&category=business&apiKey=9b583d4afb9e44fc88d4014ed00f7ac2';
 
-//contoh feth & axios
-// //fetch
-// const table = document.getElementById('data');
-// table.innerHTML = massage('loading...'); //loading muter
-// const data = fetch('https://jsonplaceholder.typicode.com/user')
 
-// data
-//     .then(res => res.json())
-//     .then(res => {
-//         table.innerHTML = render(res)
-//     })
-//     .catch (err => {
-//         table.innerHTML =massage(err.massage)
-//     })
-//     .finally(()=>{
+axios.get(apis).then(res => {
+    const {data} = res;
+    listCard(data.articles)
+});
 
-//     })
-
-// //axios (library)
-// axios.get('https://jsonplaceholder.typicode.com/user').then(res => {
-//     document.getElementById('data').innerHTML = render(res.data);
-// });
+function listCard(data) {
+    
+    let result = '';
+    console.log(data);
+    data.forEach(element => {
+    result += `
+    <div class="card m-1 mt-4" style="width: 23rem; background-color: #ced4da">
+        <img src="${element.urlToImage}" class="card-img-top" alt="">
+        <div class="card-body">
+            <h5 class="card-title">${element.title}</h5>
+            <p class="card-text">${element.publishedAt}</p>
+            <p class="card-text">${element.content}</p>
+            <a href="${element.url}" class="btn btn-outline-light bg-secondary">Read more...</a>
+        </div>
+    </div>
+    `
+});
+card.innerHTML = result;
+};
