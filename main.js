c7ab6920f1ae1b87a60b0d96f334b4f37d05be70
MrBownss <ahmadtaufik4599@gmail.com>
2023-02-15 17:09:42 +0700

    create search news

--- main.js
+++ main.js
@@ -1,29 +1,71 @@
 let card = document.getElementById('card');
-let bar = document.getElementById('searchBar');
-const apis = 'https://newsapi.org/v2/top-headlines?country=id&category=business&apiKey=9b583d4afb9e44fc88d4014ed00f7ac2';
+
+const apis = 'https://newsapi.org/v2/top-headlines?country=id&category=business&apiKey=241e074b3c3945cf850407a246c8b648';
 
 
-axios.get(apis).then(res => {
-    const {data} = res;
-    listCard(data.articles)
-});
 
-function listCard(data) {
+    axios.get(apis).then(res => {
+        const {data} = res;
+        listCard(data.articles);
+    });
     
-    let result = '';
-    console.log(data);
-    data.forEach(element => {
-    result += `
-    <div class="card m-1 mt-4" style="width: 23rem; background-color: #ced4da">
-        <img src="${element.urlToImage}" class="card-img-top" alt="">
-        <div class="card-body">
-            <h5 class="card-title">${element.title}</h5>
-            <p class="card-text">${element.publishedAt}</p>
-            <p class="card-text">${element.content}</p>
-            <a href="${element.url}" class="btn btn-outline-light bg-secondary">Read more...</a>
+    function listCard(data) {
+        let result = '';
+        console.log(data);
+        data.forEach(element => {
+        result += `
+        <div class="card m-1 mt-4" style="width: 17rem; background-color: #ced4da">
+            <img src="${element.urlToImage}" class="card-img-top" alt="">
+            <div class="card-body">
+                <h5 class="card-title">${element.title}</h5>
+                <p class="card-text">${element.publishedAt}</p>
+                <p class="card-text">${element.content}</p>
+                <a href="${element.url}" class="btn btn-outline-light bg-secondary">Read more...</a>
+            </div>
         </div>
-    </div>
-    `
-});
-card.innerHTML = result;
-};
+        `
+    });
+    card.innerHTML = result;
+    };
+
+    function searchNews() {
+
+        let input = document.getElementById('input').value
+        input = input.toLowerCase();
+        const news = `https://newsapi.org/v2/everything?q=${input}&apiKey=241e074b3c3945cf850407a246c8b648`;
+
+        axios.get(news).then(res => {
+            const {data} =  res;
+            listInput(data.articles);
+        });
+
+        function listInput(data) {
+            let result = '';
+            console.log(data);
+            data.forEach(element => {
+            result += `
+            <div class="card m-1 mt-4" style="width: 17rem; background-color: #ced4da">
+                <img src="${element.urlToImage}" class="card-img-top" alt="">
+                <div class="card-body">
+                    <h5 class="card-title">${element.title}</h5>
+                    <p class="card-text">${element.publishedAt}</p>
+                    <p class="card-text">${element.content}</p>
+                    <a href="${element.url}" class="btn btn-outline-light bg-secondary">Read more...</a>
+                </div>
+            </div>
+            `
+        });
+        card.innerHTML = result;
+        };
+    }
+
+    
+
+    
+    
+    
+
+
+
+
+
